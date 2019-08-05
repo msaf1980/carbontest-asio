f2365917608b5300216e07b0b0203bf6c70b08a8
Safronov Michail <msaf1980@gmail.com>
2019-08-05 18:55:27 +0500

    #1

--- src/runner.cpp
+++ src/runner.cpp
@@ -6,6 +6,11 @@
 #include <boost/asio/use_future.hpp>
 #include <boost/fiber/barrier.hpp>
 
+#include <log4cplus/configurator.h>
+#include <log4cplus/initializer.h>
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+
 #include <runner.hpp>
 
 using boost::thread;
@@ -15,9 +20,18 @@ using boost::fibers::barrier;
 using std::string;
 
 void runClients(const Config &config) {
-	int  threadsCount = config.Workers + config.UWorkers;
+	log4cplus::Initializer initializer;
+
+	log4cplus::BasicConfigurator logConfig;
+	logConfig.configure();
+
+	log4cplus::Logger logger =
+	    log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("main"));
+	LOG4CPLUS_WARN(logger, LOG4CPLUS_TEXT("Starting"));
+
+	int threadsCount = config.Workers + config.UWorkers;
 	Thread *threads = new Thread[threadsCount];
-	int  last = 0;
+	int last = 0;
 
 	running.store(1);
 	barrier wb(threadsCount + 1);
